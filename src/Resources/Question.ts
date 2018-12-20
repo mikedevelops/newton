18b10d6c6d03f1d2b94e06e5d8ea446f5b166008
Mike Smart <mike.smart@jadu.co.uk>
2018-12-20 21:04:35 +0000

    remove default setting, strict errors should prevent this from being possible

--- src/Resources/Question.ts
+++ src/Resources/Question.ts
@@ -4,9 +4,19 @@ import { Tag } from './Tag';
 import mongoose, { Schema } from 'mongoose';
 
 const Question = new Schema({
-    answers: [{ type: Schema.Types.ObjectId, required: true, ref: 'Answer' }],
-    utterances: [{ type: Schema.Types.ObjectId, required: true, ref: 'Utterance' }],
-    tags: [{ type: Schema.Types.ObjectId, required: true, ref: 'Tag' }]
+    answers: {
+        type: [Schema.Types.ObjectId],
+        ref: 'Answer'
+    },
+    utterances: {
+        type: [Schema.Types.ObjectId],
+        ref: 'Utterance',
+        required: true
+    },
+    tags: {
+        type: [Schema.Types.ObjectId],
+        ref: 'Tag'
+    }
 }, {
     strict: 'throw'
 });
